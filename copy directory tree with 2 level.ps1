606d1c28541b0ad589d8c20579529617a5fd737b
Aaron Law <aaronlaw@gmail.com>
2015-07-28 16:48:44 +0800

    add a script that copy directory tree structure, with security info, with no files, on 2015-05-13

---
+++ copy directory tree with 2 level.ps1
@@ -0,0 +1,16 @@
+﻿# C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe
+# Copy directory tree structure with security info (ACL), in TOP 2 level
+
+# Requirement:
+# Mirror the exact directory tree (with data) with security info (ACL),
+# while only need empty directory tree structure (do not copy any files)
+# and strict it with the TOP 2 level
+
+# Aaron Law
+# 2015-05-06
+
+# /lev:n - top n level
+# /create - copy data in 0-byte manner
+# /XF - exclude files in pattern manner
+
+robocopy "D:\PUBLIC\FILES\2_AUDIT DEPT\Yr 2015\Listed" "D:\PUBLIC\Listed-beyond-lock" /copyall /e /lev:2 /create
