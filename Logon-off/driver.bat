3e490e4644a9e3d384eff297ad301d365d393ba6
Aaron Law <aaronlaw@gmail.com>
2015-10-05 10:44:43 +0800

    past_data_2$ be set as O: drive, replacing the current past_data$

--- Logon-off/driver.bat
+++ Logon-off/driver.bat
@@ -1,8 +1,8 @@
 rem this script is stored in a GPO named 'Domain Policy', and then later be linked to cheng.local/
 net use h: \\cc-server01\public
 net use i: \\cc-server01\home
-net use o: \\cc-server01\past_data$
-rem net use m: \\cc-server01\past_data_2$
+rem net use o: \\cc-server01\past_data$
+net use o: \\cc-server01\past_data_2$
 net use /delete z:
 
 v.vbs
