409abe2fabfb1525657fb1f2a951ec76a1ea3232
Aaron Law <aaron.law@cheng.local>
2016-07-17 04:29:32 +0800

    add /attandance_server/folder_cleaner.py

---
+++ attandance_server/folder_cleaner.py
@@ -0,0 +1,13 @@
+# /bin/python3
+
+# Problem: 
+# The 701 Server program generates 2 new files every day, and so the "701 Server folder" becomes full a year later.
+
+# Requirement:
+# This script intend to clean up "the 701 server folder" by moving some old data (non-current *.msg & *.dut) to another folder.
+
+# Usage:
+# Install Python 3 and put this script into scheduler.
+
+# Author: AaronLaw
+# Last Update: 2017-07-18
