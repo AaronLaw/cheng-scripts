28f5ac52de31172e5a427b2a0705949a5320c5be
Aaron Law <aaron.law@cheng.local>
2016-07-17 04:31:33 +0800

    folder_cleaner.py moves files by file extension

--- attandance_server/folder_cleaner.py
+++ attandance_server/folder_cleaner.py
@@ -1,13 +1,25 @@
-# /bin/python3
-
+#!/usr/bin/env python3
 # Problem: 
 # The 701 Server program generates 2 new files every day, and so the "701 Server folder" becomes full a year later.
-
+#
 # Requirement:
 # This script intend to clean up "the 701 server folder" by moving some old data (non-current *.msg & *.dut) to another folder.
-
+#
 # Usage:
 # Install Python 3 and put this script into scheduler.
-
+#
 # Author: AaronLaw
-# Last Update: 2017-07-18
+# Last Update: 2017-07-17
+import os, shutil, glob
+
+source = "C:/Users/aaron.law/Sites/cheng-scripts/attandance_server"
+dist = "C:/Users/aaron.law/Sites/cheng-scripts/attandance_server/result"
+filetype = ("txt", "ps1")
+
+# Reference:
+# Stackoverflow.com: python move file ->  http://stackoverflow.com/questions/38061344/python-how-to-recursively-move-files-that-are-inside-folders
+folder = os.scandir(source) # return a list of DirEntry object
+for files in folder:
+    if files.name.endswith((filetype)):
+        print("Moving " + files.name)
+        shutil.move(os.path.join(source, files.name), os.path.join(dist, files.name))
