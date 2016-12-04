dd19a5b3fae7955d4e9866b31f344f2304e307f6
Aaron Law <aaronlaw@gmail.com>
2016-12-04 15:17:22 +0800

    Remap the O: drive to 192.168.2.194
    Remap the O: drive to 192.168.2.194. Before to do so, need to DELETE the O: frive first.

--- Logon-off/driver.bat
+++ Logon-off/driver.bat
@@ -1,12 +1,13 @@
 rem this script is stored in a GPO named 'Domain Policy', and then later be linked to cheng.local/
 net use h: \\cc-server01\public
 net use i: \\cc-server01\home
-rem net use o: \\cc-server01\past_data$
-net use o: \\cc-server01\past_data_2$
+net use /delete o:
+net use o: \\192.168.2.194\past_data_2$
+rem net use o: \\cc-server01\past_data_2$
 net use /delete z:
 
 v.vbs
 
 
 rem set default printer to Global Printer
-RUNDLL32 PRINTUI.DLL,PrintUIEntry /y /n "\\CCPRINT02\GlobalPrinter"
+rem RUNDLL32 PRINTUI.DLL,PrintUIEntry /y /n "\\CCPRINT02\GlobalPrinter"
