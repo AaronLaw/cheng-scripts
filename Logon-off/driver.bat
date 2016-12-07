75775479fb70655dc275cc87582f85e3a836e861
Aaron Law <aaronlaw@gmail.com>
2016-12-07 12:14:24 +0800

    Re-map the Past Data drive to F:\

--- Logon-off/driver.bat
+++ Logon-off/driver.bat
@@ -1,13 +1,12 @@
-rem this script is stored in a GPO named 'Domain Policy', and then later be linked to cheng.local/
+REM this script is stored in a GPO named 'Domain Policy', and then later be linked to cheng.local/
 net use h: \\cc-server01\public
 net use i: \\cc-server01\home
 net use /delete o:
-net use o: \\192.168.2.194\past_data_2$
-rem net use o: \\cc-server01\past_data_2$
+net use o: \\cc-server01\past_data_2$
 net use /delete z:
 
 v.vbs
 
 
-rem set default printer to Global Printer
-rem RUNDLL32 PRINTUI.DLL,PrintUIEntry /y /n "\\CCPRINT02\GlobalPrinter"
+REM set default printer to Global Printer
+REM RUNDLL32 PRINTUI.DLL,PrintUIEntry /y /n "\\CCPRINT02\GlobalPrinter"
