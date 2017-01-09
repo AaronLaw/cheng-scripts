f8ff11c52eb13a083db4c9347c7f243c2f0a6f08
David Leung <davidleungcm@gmail.com>
2017-01-09 14:42:16 +0800

    driver.bat is no longer in use.

--- Logon-off/driver.bat
+++ Logon-off/driver.bat
@@ -1,4 +1,5 @@
 REM this script is stored in a GPO named 'Domain Policy', and then later be linked to cheng.local/
+REM 2017-01-09: This script is no longer in use. We set drive mapping in 'Domain Policy -> User Configuration -> Preferences -> Windows Settings -> Drive Maps' in GPO instead.
 net use h: \\cc-server01\public
 net use i: \\cc-server01\home
 net use /delete o:
