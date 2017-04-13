945e593cbf3f79e60f5cf7475abf50f7e8ea1dde
Aaron Law <aaron.law@cheng.local>
2017-04-13 11:19:34 +0800

    Implement the basic function of change-file-timestamp.py

---
+++ File system related/change-file-timestamp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Problem:
+# need to change modification time ( timestamp) on folders
+
+# Usage:
+#
+# ref:
+# Google: python 3 change file timestamp -> http://www.dreamincode.net/forums/topic/307923-change-timestamp-on-file-to-current-time/
+
+
+# Author: AaronLaw
+# Last Update: 2017-04-13
+import os, time
+from stat import *
+
+# returns a list of all files on the current directory
+files = os.scandir('.')
+os.chdir('.')
+
+for f in files:
+    print(f)
+    if f.is_dir():
+        st = os.stat(f)
+        atime = st[ST_ATIME] #access time
+        mtime = st[ST_MTIME] #modification time
+		
+        new_mtime = mtime+(4*3600)
+		
+        #modify the file timestamp
+        os.utime(f, (atime, new_mtime))
